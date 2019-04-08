c37078da3ed7d97a635b547a14ed3d9a9b87fdc9
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-08 15:03:43 -0700

    Displays tasks on /tasks path

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+
+  TASKS = [
+    {
+      task: "Hikefinder exercise Section 1", status: [:complete, :incomplete] 
+    },
+        {
+      task: "Make lunch for the week", status: [:complete, :incomplete] 
+    },
+        {
+      task: "Watch short video on MVC", status: [:complete, :incomplete] 
+    },
+
+  ]
+  def index
+    @tasks = TASKS;
+  end
+end
